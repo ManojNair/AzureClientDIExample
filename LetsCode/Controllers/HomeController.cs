3749535042932b3ad00eaf6071dcc11a5a5359be
Manoj Nair <Manoj.Nair@outlook.com>
2018-02-08 13:44:48 +1100

    Add project files.

---
+++ LetsCode/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LetsCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAzureClient _azureClient;
+
+        public HomeController(IAzureClient azureClient)
+        {
+            _azureClient = azureClient;
+        }
+
+       [HttpGet("/api/virtualmachines")]
+        public JsonResult Index()
+       {
+           var vms = _azureClient.GetClient().VirtualMachines.List().ToList().Select(s => s.Name);
+           return Json(vms, new JsonSerializerSettings() {Formatting = Formatting.Indented});
+       }
+    }
+}
---
+++ LetsCode/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using static Microsoft.Azure.Management.ResourceManager.Fluent.Core.HttpLoggingDelegatingHandler;
+
+namespace LetsCode
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup()
+        {
+            
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IAzureClient, AzureClient>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IAzureClient client)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                
+                var azureClient = client.GetClient();
+
+                var vms = azureClient.VirtualMachines.List().ToList()[0];
+
+
+                await context.Response.WriteAsync(vms.Name);
+            });
+        }
+    }
+
+    public interface IAzureClient
+    {
+      //  AzureCredentials GetCredentials();
+        
+        
+        IAzure GetClient();
+    }
+
+    public class AzureClient : IAzureClient
+    {
+        private readonly AzureCredentials _credentials;
+        // public AzureCredentials Credentials = SdkContext.AzureCredentialsFactory.FromFile(@"auth.json");
+
+        public AzureClient()
+        {
+            _credentials = SdkContext.AzureCredentialsFactory.FromFile(@"auth.json");
+        }
+
+        
+
+        public IAzure GetClient() => Azure.Configure()
+            .WithLogLevel(Level.BodyAndHeaders)
+            .Authenticate(_credentials).WithDefaultSubscription();
+    }
+}
